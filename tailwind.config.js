4f69f858a656572281fa1e06dcd4e8b851b19edc
Denis <denokim55@gmail.com>
2023-04-19 10:55:33 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    screens: {
+      xs: "320px",
+      // => @media (min-width: 320px) { ... }
+
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
+    },
+    extend: {
+      colors: {
+        "changa-green": {
+          50: "#F4FFDB",
+          300: "#9FE870",
+          400: "#9BE06E",
+          500: "#84BD00",
+          600: "#84C041",
+        },
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/forms")],
+};
